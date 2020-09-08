260535776dcbbd6160884d3ff44967c3639ac9dc
Tufan Deveci <info@tufandeveci.com.tr>
2020-09-09 01:23:22 +0300

    ubuntu 20.04

--- scripts/install-libreoffice.sh
+++ scripts/install-libreoffice.sh
@@ -22,14 +22,15 @@ apt-get -y install ca-certificates
 
 # install ssh-keygen binary for the WOPI proof key
 apt-get -y install openssh-client
+apt-get -y install dirmngr
 
 # Add Collabora repos
-echo "deb https://collaboraoffice.com/repos/CollaboraOnline/CODE-ubuntu1804 /" > /etc/apt/sources.list.d/collabora.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6CCEA47B2281732DF5D504D00C54D189F4BA284D
+echo 'deb https://www.collaboraoffice.com/repos/CollaboraOnline/CODE-ubuntu2004 ./' >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0C54D189F4BA284D
 apt-get update
 
 # Install the Collabora packages
-apt-get -y install loolwsd code-brand collaboraoffice6.2-dict* collaboraofficebasis6.2-ar collaboraofficebasis6.2-as collaboraofficebasis6.2-ast collaboraofficebasis6.2-bg collaboraofficebasis6.2-bn-in collaboraofficebasis6.2-br collaboraofficebasis6.2-ca collaboraofficebasis6.2-calc collaboraofficebasis6.2-ca-valencia collaboraofficebasis6.2-core collaboraofficebasis6.2-cs collaboraofficebasis6.2-cy collaboraofficebasis6.2-da collaboraofficebasis6.2-de collaboraofficebasis6.2-draw collaboraofficebasis6.2-el collaboraofficebasis6.2-en-gb collaboraofficebasis6.2-en-us collaboraofficebasis6.2-es collaboraofficebasis6.2-et collaboraofficebasis6.2-eu collaboraofficebasis6.2-extension-pdf-import collaboraofficebasis6.2-fi collaboraofficebasis6.2-fr collaboraofficebasis6.2-ga collaboraofficebasis6.2-gd collaboraofficebasis6.2-gl collaboraofficebasis6.2-graphicfilter collaboraofficebasis6.2-gu collaboraofficebasis6.2-he collaboraofficebasis6.2-hi collaboraofficebasis6.2-hr collaboraofficebasis6.2-hu collaboraofficebasis6.2-id collaboraofficebasis6.2-images collaboraofficebasis6.2-impress collaboraofficebasis6.2-is collaboraofficebasis6.2-it collaboraofficebasis6.2-ja collaboraofficebasis6.2-km collaboraofficebasis6.2-kn collaboraofficebasis6.2-ko collaboraofficebasis6.2-lt collaboraofficebasis6.2-lv collaboraofficebasis6.2-ml collaboraofficebasis6.2-mr collaboraofficebasis6.2-nb collaboraofficebasis6.2-nl collaboraofficebasis6.2-nn collaboraofficebasis6.2-oc collaboraofficebasis6.2-ooofonts collaboraofficebasis6.2-ooolinguistic collaboraofficebasis6.2-or collaboraofficebasis6.2-pa-in collaboraofficebasis6.2-pl collaboraofficebasis6.2-pt collaboraofficebasis6.2-pt-br collaboraofficebasis6.2-ro collaboraofficebasis6.2-ru collaboraofficebasis6.2-sk collaboraofficebasis6.2-sl collaboraofficebasis6.2-sr collaboraofficebasis6.2-sr-latn collaboraofficebasis6.2-sv collaboraofficebasis6.2-ta collaboraofficebasis6.2-te collaboraofficebasis6.2-tr collaboraofficebasis6.2-uk collaboraofficebasis6.2-vi collaboraofficebasis6.2-writer collaboraofficebasis6.2-zh-cn collaboraofficebasis6.2-zh-tw
+apt-get update && apt-get install -y loolwsd code-brand collaboraofficebasis6.2-tr ollaboraoffice6.2-dict* 
 
 # Install inotifywait and killall to automatic restart loolwsd, if loolwsd.xml changes
 apt-get -y install inotify-tools psmisc
